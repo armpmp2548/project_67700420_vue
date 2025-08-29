bf8c2bec1384f44b72df1432ca8c1f79b6c143a1
ชื่armpmp2548 <phumiphatbudnuch@gmail.com>
2025-08-29 11:35:43 -0700

    update v1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
-<template>
- 
- <nav class="navbar navbar-expand-lg " style="background-color: #e3f2fd;"> 
-  <div class="container">
+<template lang="">
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -9,24 +9,24 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/showproduct">Show Product</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">login</a></li>
-            <li><a class="dropdown-item" href="#">logout</a></li>
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Register</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-       <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -40,8 +40,15 @@
 
 
 
-
-
-  <router-view/>
+    </div>
 </template>
 
+
+
+
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/ShowProduct.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container my-5">
+    <h2 class="text-center mb-4">รายการสินค้า</h2>
+    <div class="row">
+      <div class="col-md-4" v-for="product in products" :key="product.id">
+        <div class="card shadow-sm mb-4">
+          <img :src="product.image" class="card-img-top" :alt="product.name">
+          <div class="card-body text-center">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">{{ product.price }} บาท</p>
+            <button class="btn btn-primary">รายละเอียด</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowProduct",
+  data() {
+    return {
+      products: [
+        { id: 1, name: "เตาไฟฟ้า", price: 1200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwaT_gNNgPvXQPUszhjyGnp6x5SrTIyGM4gg&s" },
+        { id: 2, name: "หม้อชาบู", price: 800, image: "https://backend.sinthanee.com/uploads/products/NewFolder/33.png" },
+        { id: 3, name: "ชุดช้อนส้อม", price: 250, image: "https://www.verasu.com/media/catalog/product/cache/9fe915041f3f0fb5ee2faedd41ee1b2f/a/m/ame-austin24_01_02.jpg" },
+        { id: 1, name: "เตียงนอน", price: 5000, image: "https://assets.mattresscity.co.th/media/images/e0f8212d-c573-4cda-8b5a-53199ac16309/cristalgrey.jpg" },
+        { id: 2, name: "พัดลม", price: 800, image: "https://kashiwa.co.th/wp-content/uploads/2025/03/FS-2201.webp" },
+        { id: 3, name: "ตู้เสื้อผ้า", price: 2500, image: "https://www.iconic-office.com/wp-content/uploads/2017/12/GreyBG-WS-C2-2.png" }
+      ]
+    }
+  }
+}
+</script>
